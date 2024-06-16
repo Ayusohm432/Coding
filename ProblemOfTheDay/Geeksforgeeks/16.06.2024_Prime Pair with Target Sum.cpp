e8abaac5731d05ec76879391e816d807b3debef9
ayusohm432 <ayushkumar76174@gmail.com>
2024-06-16 20:52:25 +0530

    ProblemOfTheDay Solved !..................

---
+++ ProblemOfTheDay/Geeksforgeeks/16.06.2024_Prime Pair with Target Sum.cpp
@@ -0,0 +1,94 @@
+/*
+problem Link :- https://www.geeksforgeeks.org/problems/sum-of-prime4751/1
+*/
+
+/*Prime Pair with Target Sum
+
+Given a number n, find out if n can be expressed as a+b, where both a and b are prime numbers. If such a pair exists, return the values of a and b, otherwise return [-1,-1] as an array of size 2.
+Note: If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, and a < c then  [a, b] is considered as our answer.
+
+Examples
+
+Input: n = 10
+Output: 3 7
+Explanation: There are two possiblities 3, 7 & 5, 5 are both prime & their sum is 10, but we'll pick 3, 7 as 3 < 5.
+Input: n = 3
+Output: -1 -1
+Explanation: There are no solutions to the number 3.
+Expected Time Complexity: O(n*loglog(n))
+Expected Auxiliary Space: O(n)
+
+Constraints:
+2 <= n <= 106
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> getPrimes(int n) {
+        // code here
+        bool prime[n+1];
+        memset(prime,true,sizeof(prime));
+        prime[0] = false;
+        prime[1] = false;
+        for(int i=2 ; i*i<=n ; i++){
+            if(prime[i]){
+                for(int j=i*i ; j<=n ; j+=i){
+                    prime[j] = false;
+                }
+            }
+        }
+        
+        for(int i=2 ; i<=n ; i++){
+            if(prime[i] and prime[n-i]){
+                return {i,n-i};
+            }
+        }
+        return {-1,-1};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        vector<int> res = obj.getPrimes(n);
+
+        Array::print(res);
+    }
+}
+
+// } Driver Code Ends
