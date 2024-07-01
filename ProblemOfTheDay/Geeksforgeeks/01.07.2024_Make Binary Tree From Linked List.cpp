f5837aa58311c597881a3c9ecdab8ec430ca0d47
ayusohm432 <ayushkumar76174@gmail.com>
2024-07-01 18:14:31 +0530

    ProblemOfTheDay Solved!....

---
+++ ProblemOfTheDay/Geeksforgeeks/01.07.2024_Make Binary Tree From Linked List.cpp
@@ -0,0 +1,172 @@
+/*
+Problem Link :- https://www.geeksforgeeks.org/problems/make-binary-tree/1
+*/
+
+/*Make Binary Tree From Linked List
+
+Given a Linked List Representation of Complete Binary Tree. Your task is to construct the Binary tree from the given linkedlist and return the root of the tree.
+The result will be judged by printing the level order traversal of the Binary tree. 
+Note: The complete binary tree is represented as a linked list in a way where if the root node is stored at position i, its left, and right children are stored at position 2*i+1, and 2*i+2 respectively. H is the height of the tree and this space is used implicitly for the recursion stack.
+
+Examples:
+
+Input: list = 1->2->3->4->5
+Output: 1 2 3 4 5
+Explanation: The tree would look like
+      1
+    /   \
+   2     3
+ /  \
+4   5
+Now, the level order traversal of
+the above tree is 1 2 3 4 5.
+Input: list = 5->4->3->2->1
+Output: 5 4 3 2 1
+Explanation: The tree would look like
+     5
+   /  \
+  4    3
+ / \
+2   1
+Now, the level order traversal of
+the above tree is 5 4 3 2 1.
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+Constraints:
+1 <= size(list) <= 105
+1 <= node.data <= 105
+
+
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void push(struct Node **head_ref, int new_data) {
+
+    struct Node *new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void lvl(TreeNode *r) {
+    if (r == NULL)
+        return;
+    queue<TreeNode *> q;
+    q.push(r);
+    while (!q.empty()) {
+        TreeNode *j = q.front();
+        cout << j->data << " ";
+        q.pop();
+        if (j->left)
+            q.push(j->left);
+        if (j->right)
+            q.push(j->right);
+    }
+}
+
+void reverses(Node **head) {
+    Node *prev = NULL;
+    Node *cur = *head;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *head = prev;
+}
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    TreeNode* buildTree(vector<int>& arr , int i){
+        if(i>=arr.size()){
+            return nullptr;
+        }
+        
+        TreeNode* newNode = new TreeNode(arr[i]);
+        newNode->left = buildTree(arr,i*2+1);
+        newNode->right = buildTree(arr,i*2+2);
+        
+        return newNode;
+    }
+    void convert(Node *head, TreeNode *&root) {
+        vector<int> arr;
+
+        while(head){
+            arr.push_back(head->data);
+            head = head->next;
+        }
+        
+        root = buildTree(arr,0);
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T, i, n, l, k;
+
+    cin >> T;
+
+    while (T--) {
+        struct Node *head = NULL;
+
+        cin >> n;
+        for (i = 1; i <= n; i++) {
+            cin >> l;
+            push(&head, l);
+        }
+        reverses(&head);
+        Node *prevhead = head;
+        TreeNode *root = NULL;
+        Solution obj;
+
+        obj.convert(head, root);
+        if (root == NULL) {
+            cout << "-1\n";
+        } else if (root == NULL && prevhead != NULL) {
+            cout << "-1\n";
+        } else {
+            lvl(root);
+            cout << endl;
+        }
+        // inorder(root);
+        getchar();
+    }
+    return 0;
+}
+
+// } Driver Code Ends
