24a6a417949f8340f1ee60f579a9cb983eb61f2d
ayusohm432 <ayushkumar76174@gmail.com>
2024-08-05 22:18:53 +0530

    ProblemOfTheDay Solved !....

---
+++ ProblemOfTheDay/Geeksforgeeks/05.08.2024_Bottom View of Binary Tree.cpp
@@ -0,0 +1,205 @@
+/*
+Problem Link :- https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+*/
+
+/*Bottom View of Binary Tree
+
+Given a binary tree, return an array where elements represent the bottom view of the binary tree from left to right.
+
+Note: If there are multiple bottom-most nodes for a horizontal distance from the root, then the latter one in the level traversal is considered. For example, in the below diagram, 3 and 4 are both the bottommost nodes at a horizontal distance of 0, here 4 will be considered.
+
+                      20
+                    /    \
+                  8       22
+                /   \     /   \
+              5      3 4     25
+                     /    \      
+                 10       14
+
+For the above tree, the output should be 5 10 4 14 25.
+
+Examples :
+
+Input:
+       1
+     /   \
+    3     2
+Output: 3 1 2
+Explanation: First case represents a tree with 3 nodes and 2 edges where root is 1, left child of 1 is 3 and right child of 1 is 2.
+
+Thus bottom view of the binary tree will be 3 1 2.
+Input:
+         10
+       /    \
+      20    30
+     /  \
+    40   60
+Output: 40 20 60 30
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+
+
+*/
+
+//{ Driver Code Starts
+// #include <bits/stdc++.h>
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+#include<string>
+#include<sstream>
+
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+vector <int> bottomView(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Function to return a list containing the bottom view of the given tree.
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        vector<int> ans;
+        queue<pair<Node* , int>> q;
+        unordered_map<int , int > mp;
+        
+        q.push({root , 0});
+        
+        int mn = 0 , mx = 0;
+        while(!q.empty()){
+            auto a = q.front();
+            q.pop();
+            Node *temp = a.first;
+            mp[a.second] = temp->data;
+            mn = min(mn , a.second);
+            mx = max(mx , a.second);
+            if(temp->left){
+                q.push({temp->left , a.second-1});
+            }
+            if(temp->right){
+                q.push({temp->right , a.second+1});
+            }
+        }
+        
+        for(int i=mn ; i<=mx ; i++){
+            ans.push_back(mp[i]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.bottomView(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
