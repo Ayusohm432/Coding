35dd0b83f57a2297f4e03a37489141d91f768f7f
ayusohm432 <ayushkumar76174@gmail.com>
2024-08-23 11:27:14 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Leetcode/23.08.2024_l592. Fraction Addition and Subtraction.cpp
@@ -0,0 +1,100 @@
+/*
+Problem Link :- https://leetcode.com/problems/fraction-addition-and-subtraction/description/?envType=daily-question&envId=2024-08-23
+*/
+
+/*592. Fraction Addition and Subtraction
+
+
+Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
+
+The final result should be an irreducible fraction. If your final result is an integer, change it to the format of a fraction that has a denominator 1. So in this case, 2 should be converted to 2/1.
+
+ 
+
+Example 1:
+
+Input: expression = "-1/2+1/2"
+Output: "0/1"
+Example 2:
+
+Input: expression = "-1/2+1/2+1/3"
+Output: "1/3"
+Example 3:
+
+Input: expression = "1/3-1/2"
+Output: "-1/6"
+ 
+
+Constraints:
+
+The input string only contains '0' to '9', '/', '+' and '-'. So does the output.
+Each fraction (input and output) has the format ±numerator/denominator. If the first input fraction or the output is positive, then '+' will be omitted.
+The input only contains valid irreducible fractions, where the numerator and denominator of each fraction will always be in the range [1, 10]. If the denominator is 1, it means this fraction is actually an integer in a fraction format defined above.
+The number of given fractions will be in the range [1, 10].
+The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
+
+
+*/
+#include <iostream>
+#include<string>
+#include<numeric>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        int n = expression.length();
+        
+        int num = 0;
+        int den = 1;
+
+        int i=0;
+        while(i<n){
+            int currnum = 0;
+            int currden = 0;
+
+            bool isNeg = (expression[i] == '-');
+
+            if(expression[i] == '+' || expression[i] == '-'){
+                i++;
+            }
+
+            while(i<n && isdigit(expression[i])){
+                int val = expression[i] - '0';
+                currnum = (currnum * 10 ) + val;
+                i++;
+            }
+            i++;
+
+            if(isNeg == true){
+                currnum *= -1;
+            }
+
+            while(i<n && isdigit(expression[i])){
+                int val = expression[i] - '0';
+                currden = (currden * 10 ) + val;
+                i++;
+            }
+
+            num = num * currden + currnum * den;
+            den = den * currden;
+        }
+
+        int Gcd = abs(__gcd(num , den));
+
+        num /= Gcd;
+        den /= Gcd;
+
+        return to_string(num) + "/" + to_string(den);
+    }
+};
+
+
+int main() {
+    Solution solution;
+    string expression = "1/2+1/3";
+    cout << solution.fractionAddition(expression) << endl;
+    return 0;
+}
