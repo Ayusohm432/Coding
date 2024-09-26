622d7d8f0888b27387ab162c5347f96dc035ff0f
ayusohm432 <ayushkumar76174@gmail.com>
2024-09-26 08:21:59 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Leetcode/26.09.2024_729. My Calendar I.cpp
@@ -0,0 +1,65 @@
+/*
+Problem Link : https://leetcode.com/problems/my-calendar-i/description/?envType=daily-question&envId=2024-09-26
+*/
+
+/*729. My Calendar I
+
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendar class:
+
+MyCalendar() Initializes the calendar object.
+boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+ 
+
+Constraints:
+
+0 <= start < end <= 109
+At most 1000 calls will be made to book.
+
+*/
+
+class MyCalendar {
+    set<pair<int, int>> calendar;
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        auto it = calendar.lower_bound({start, end});
+        if (it != calendar.begin() && prev(it)->second > start) {
+            return false;
+        }
+        if (it != calendar.end() && it->first < end) {
+            return false;
+        }
+        calendar.insert(it, {start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
