02cb3c7cda4b9e5fdf85e377a582cfe98ec6c2b7
ayusohm432 <ayushkumar76174@gmail.com>
2024-11-03 14:15:51 +0530

    ProblemOfTheDay Solved !...

---
+++ ProblemOfTheDay/Geeksforgeeks/03.11.2024_Is Linked List Length Even.cpp
@@ -0,0 +1,40 @@
+/*
+Problem Link : https://www.geeksforgeeks.org/problems/linked-list-length-even-or-odd/1
+*/
+
+/*Is Linked List Length Even?
+
+Given a linked list, your task is to complete the function isLengthEven() which contains the head of the linked list, and check whether the length of the linked list is even or not. Return true if it is even, otherwise false.
+
+Examples:
+
+Input: Linked list: 12->52->10->47->95->0
+
+Output: true
+Explanation: The length of the linked list is 6 which is even, hence returned true.
+Input: Linked list: 9->4->3
+
+Output: false
+Explanation: The length of the linked list is 3 which is odd, hence returned false.
+Expected Time Complexity: O(n)
+Expected Auxillary Space: O(1)
+
+Constraints:
+1 <= number of nodes <= 105
+1 <= elements of the linked list <= 105
+
+*/
+class Solution {
+  public:
+    bool isLengthEven(struct Node **head) {
+        // Code here
+        Node *temp = *head;
+        int count = 0;
+        while(temp){
+            count++;
+            temp = temp->next;
+        }
+        
+        return !(count % 2);
+    }
+};
