0636db2dd65ce1f564601a55249f265862a67db6
ayusohm432 <ayushkumar76174@gmail.com>
2024-12-14 20:31:06 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Leetcode/14.12.2024_2762. Continuous Subarrays.cpp
@@ -0,0 +1,113 @@
+/*
+Problem Link: https://leetcode.com/problems/continuous-subarrays?envType=daily-question&envId=2024-12-14
+*/
+
+/*2762. Continuous Subarrays
+
+You are given a 0-indexed integer array nums. A subarray of nums is called continuous if:
+
+Let i, i + 1, ..., j be the indices in the subarray. Then, for each pair of indices i <= i1, i2 <= j, 0 <= |nums[i1] - nums[i2]| <= 2.
+Return the total number of continuous subarrays.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [5,4,2,4]
+Output: 8
+Explanation: 
+Continuous subarray of size 1: [5], [4], [2], [4].
+Continuous subarray of size 2: [5,4], [4,2], [2,4].
+Continuous subarray of size 3: [4,2,4].
+Thereare no subarrys of size 4.
+Total continuous subarrays = 4 + 3 + 1 = 8.
+It can be shown that there are no more continuous subarrays.
+ 
+
+Example 2:
+
+Input: nums = [1,2,3]
+Output: 6
+Explanation: 
+Continuous subarray of size 1: [1], [2], [3].
+Continuous subarray of size 2: [1,2], [2,3].
+Continuous subarray of size 3: [1,2,3].
+Total continuous subarrays = 3 + 2 + 1 = 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+
+*/
+
+//Approach-1
+class Solution {
+public:
+    #define P pair<int, int>
+    long long continuousSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        priority_queue<P, vector<P>, greater<P>> minHeap;
+        priority_queue<P> maxHeap;
+
+        int i = 0;
+        int j = 0;
+        long long count = 0;
+
+        while(j < n){
+            minHeap.push({nums[j], j});
+            maxHeap.push({nums[j], j});
+
+            while(abs(maxHeap.top().first - minHeap.top().first) > 2){
+                i++;
+                
+                while(!maxHeap.empty() && maxHeap.top().second < i){
+                    maxHeap.pop();
+                }
+
+                while(!minHeap.empty() && minHeap.top().second < i){
+                    minHeap.pop();
+                }
+            }
+
+            count += j - i + 1;
+            j++;
+        }
+
+        return count;
+    }
+}; 
+
+//Approach-2
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        map<int, int> mp;
+
+        int i = 0;
+        int j = 0;
+
+        long long count = 0;
+
+        while(j < n){
+            mp[nums[j]]++;
+            while(abs(mp.rbegin()->first - mp.begin()->first) > 2){
+                mp[nums[i]] -= 1;
+                if(mp[nums[i]] == 0){
+                    mp.erase(nums[i]);
+                }
+                i++;
+            }
+
+            count += j - i + 1;
+            j++;
+        }
+        return count;
+    }
+};
