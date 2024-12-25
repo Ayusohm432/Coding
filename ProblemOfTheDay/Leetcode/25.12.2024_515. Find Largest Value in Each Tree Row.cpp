8e7f294b1ec8b2be5f4fe3c2b5b2ffb1d74427ce
ayusohm432 <ayushkumar76174@gmail.com>
2024-12-25 17:01:20 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Leetcode/25.12.2024_515. Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,139 @@
+/*
+Problem link: https://leetcode.com/problems/find-largest-value-in-each-tree-row?envType=daily-question&envId=2024-12-25
+*/
+
+/*515. Find Largest Value in Each Tree Row
+
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+ 
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+ 
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-231 <= Node.val <= 231 - 1
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//Approach - 1
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if (root == nullptr) {
+            return vector<int>{};
+        }
+        
+        vector<int> ans;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        
+        while (!queue.empty()) {
+            int currentLength = queue.size();
+            int currMax = INT_MIN;
+            
+            for (int i = 0; i < currentLength; i++) {
+                TreeNode* node = queue.front();
+                queue.pop();
+                currMax = max(currMax, node->val);
+                
+                if (node->left) {
+                    queue.push(node->left);
+                }
+                
+                if (node->right) {
+                    queue.push(node->right);
+                }
+            }
+            
+            ans.push_back(currMax);
+        }
+        
+        return ans;
+    }
+};
+
+//Approach - 2
+class Solution {
+public:
+    vector<int> ans;
+    
+    vector<int> largestValues(TreeNode* root) {
+        dfs(root, 0);
+        return ans;
+    }
+    
+    void dfs(TreeNode* node, int depth) {
+        if (node == nullptr) {
+            return;
+        }
+        
+        if (depth == ans.size()) {
+            ans.push_back(node->val);
+        } else {
+            ans[depth] = max(ans[depth], node->val);
+        }
+        
+        dfs(node->left, depth + 1);
+        dfs(node->right, depth + 1);
+    }
+};
+
+//Approach - 3
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if (root == nullptr) {
+            return vector<int>{};
+        }
+        
+        vector<int> ans;
+        stack<pair<TreeNode*, int>> stack;
+        stack.push(make_pair(root, 0));
+        
+        while (!stack.empty()) {
+            pair<TreeNode*, int> pair = stack.top();
+            stack.pop();
+            TreeNode* node = pair.first;
+            int depth = pair.second;
+            
+            if (depth == ans.size()) {
+                ans.push_back(node->val);
+            } else {
+                ans[depth] = max(ans[depth], node->val);
+            }
+            
+            if (node->left) {
+                stack.push(make_pair(node->left, depth + 1));
+            }
+            
+            if (node->right) {
+                stack.push(make_pair(node->right, depth + 1));
+            }
+        }
+        
+        return ans;
+    }
+};
