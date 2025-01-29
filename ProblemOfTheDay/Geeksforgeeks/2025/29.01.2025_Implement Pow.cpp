3cdc77dc930a63d3764e8c90965d7a46f6441601
ayusohm432 <ayushkumar76174@gmail.com>
2025-01-29 08:47:43 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Geeksforgeeks/2025/29.01.2025_Implement Pow.cpp
@@ -0,0 +1,45 @@
+/*
+Problem link: https://www.geeksforgeeks.org/problems/powx-n/1
+*/
+
+/*Implement Pow
+
+Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+
+Examples:
+
+Input: b = 3.00000, e = 5
+Output: 243.00000
+Input: b = 0.55000, e = 3
+Output: 0.16638
+Input: b = -0.67000, e = -7
+Output: -16.49971
+Constraints:
+
+-100.0 < b < 100.0
+-109 <= e <= 109
+Either b is not zero or e > 0.
+-104 <= be <= 104
+
+*/
+
+class Solution {
+  public:
+    double power(double b, int e) {
+        if (e < 0) {
+            return 1 / power(b, -e);
+        }
+        if (e == 1) {
+            return b;
+        }
+        if (e == 0) {
+            return 1;
+        }
+
+        double halfPow = power(b, e / 2);
+        if (e % 2 == 0) {
+            return halfPow * halfPow;
+        }
+        return b * halfPow * halfPow;
+    }
+};
