185f2aea10eddc420e0ea15cc24dc3c66028ea0e
ayusohm432 <ayushkumar76174@gmail.com>
2025-05-28 08:17:53 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Geeksforgeeks/2025/5.May/28.05.2025_Find rectangle with corners as 1.cpp
@@ -0,0 +1,55 @@
+/*
+Problem Link: https://www.geeksforgeeks.org/problems/find-rectangle-with-corners-as-1--141631/1
+*/
+
+/*Find rectangle with corners as 1
+
+Given an n x m binary matrix mat[][] containing only 0s and 1s, determine if there exists a rectangle within the matrix such that all four corners of the rectangle are 1. If such a rectangle exists, return true; otherwise, return false.
+
+Example:
+
+Input: mat[][] =
+[[1, 0, 0, 1, 0],
+[0, 0, 1, 0, 1],
+[0, 0, 0, 1, 0], 
+[1, 0, 1, 0, 1]] 
+Output: true
+Explanation: Valid corners are at index (1,2), (1,4), (3,2), (3,4) 
+Input:mat[][] =
+[[0, 0, 0],
+[0, 0, 0],
+[0, 0, 0]]
+Output: false
+Explanation: There are no valid corners.
+Constraints:
+1 <= n, m <= 200
+0 <= mat[i] <= 1
+
+*/
+
+class Solution {
+  public:
+    bool ValidCorner(vector<vector<int> >& mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1){
+                    for(int k=i+1; k<n; k++){
+                        if(mat[k][j] == 1){
+                            for(int l=j+1; l<m; l++){
+                                if(mat[i][l] == 1 && mat[k][l] == 1){
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+};
