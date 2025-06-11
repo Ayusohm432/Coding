10edc1f3971933a5614f1bca0c3a283babdf4b3b
ayusohm432 <ayushkumar76174@gmail.com>
2025-06-11 11:44:29 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Geeksforgeeks/2025/6. June/11.06.2025_Remove the balls.cpp
@@ -0,0 +1,47 @@
+/*
+Problem Link: https://www.geeksforgeeks.org/problems/remove-the-balls--170647/1
+*/
+
+/*Remove the balls
+
+You are given two arrays, color and radius, representing a sequence of balls:
+
+color[i] is the color of the i-th ball.
+
+radius[i] is the radius of the i-th ball.
+
+If two consecutive balls have the same color and radius, remove them both. Repeat this process until no more such pairs exist.
+
+
+Return the number of balls remaining after all possible removals.
+
+Examples:
+
+Input: color[] = [2, 3, 5], radius[] = [3, 3, 5]
+Output: 3
+Explanation: All the 3 balls have different colors and radius.
+Input: color[] = [2, 2, 5], radius[] = [3, 3, 5]
+Output: 1
+Explanation: First ball and second ball have same color 2 and same radius 3. So, after removing only one ball is left. It cannot be removed from the array. Hence, the final array has length 1.
+Constraints:
+1 ≤ color.size() = radius.size() ≤ 105
+1 ≤ color[i] ≤ 109
+1 ≤ radius[i] ≤ 109                                                                                                                                           
+
+*/
+class Solution {
+  public:
+    int findLength(vector<int> &color, vector<int> &radius) {
+        // code here
+        int n = color.size();
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            if(!st.empty() && color[i] == color[st.top()] && radius[i] == radius[st.top()]){
+                st.pop();
+            }else{
+                st.push(i);
+            } 
+        }
+        return st.size();
+    }
+};
