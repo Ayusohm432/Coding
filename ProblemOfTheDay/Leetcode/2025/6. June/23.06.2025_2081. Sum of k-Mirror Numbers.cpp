b2255211d460afd0cc6655f89ff536eb2da9770f
ayusohm432 <ayushkumar76174@gmail.com>
2025-06-24 13:39:55 +0530

    ProblemOfTheDay solved !...

---
+++ ProblemOfTheDay/Leetcode/2025/6. June/23.06.2025_2081. Sum of k-Mirror Numbers.cpp
@@ -0,0 +1,123 @@
+/*
+Problem Link: https://leetcode.com/problems/sum-of-k-mirror-numbers?envType=daily-question&envId=2025-06-23
+*/
+
+/*2081. Sum of k-Mirror Numbers
+
+A k-mirror number is a positive integer without leading zeros that reads the same both forward and backward in base-10 as well as in base-k.
+
+For example, 9 is a 2-mirror number. The representation of 9 in base-10 and base-2 are 9 and 1001 respectively, which read the same both forward and backward.
+On the contrary, 4 is not a 2-mirror number. The representation of 4 in base-2 is 100, which does not read the same both forward and backward.
+Given the base k and the number n, return the sum of the n smallest k-mirror numbers.
+
+ 
+
+Example 1:
+
+Input: k = 2, n = 5
+Output: 25
+Explanation:
+The 5 smallest 2-mirror numbers and their representations in base-2 are listed as follows:
+  base-10    base-2
+    1          1
+    3          11
+    5          101
+    7          111
+    9          1001
+Their sum = 1 + 3 + 5 + 7 + 9 = 25. 
+Example 2:
+
+Input: k = 3, n = 7
+Output: 499
+Explanation:
+The 7 smallest 3-mirror numbers are and their representations in base-3 are listed as follows:
+  base-10    base-3
+    1          1
+    2          2
+    4          11
+    8          22
+    121        11111
+    151        12121
+    212        21212
+Their sum = 1 + 2 + 4 + 8 + 121 + 151 + 212 = 499.
+Example 3:
+
+Input: k = 7, n = 17
+Output: 20379000
+Explanation: The 17 smallest 7-mirror numbers are:
+1, 2, 3, 4, 5, 6, 8, 121, 171, 242, 292, 16561, 65656, 2137312, 4602064, 6597956, 6958596
+ 
+
+Constraints:
+
+2 <= k <= 9
+1 <= n <= 30
+
+*/
+
+class Solution {
+    bool isPalindrome(string str){
+        int i = 0;
+        int j = str.length() - 1;
+
+        while(i <= j){
+            if(str[i] != str[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    string convertToBaseK(long long num, int k){
+        if(num == 0){
+            return "0";
+        }
+
+        string result = "";
+        while(num > 0){
+            result += to_string(num % k);
+            num /= k;
+        }
+        return result;
+    }
+public:
+    long long kMirror(int k, int n) {
+        long long sum = 0;
+        int len = 1;
+
+        while(n > 0){
+            int halfLength = (len+1) / 2;
+
+            long long minNum = pow(10, halfLength-1);
+            long long maxNum = pow(10, halfLength) - 1;
+
+            for(int num=minNum; num<=maxNum; num++){
+                string firstHalf = to_string(num);
+                string secondHalf = firstHalf;
+                reverse(secondHalf.begin(), secondHalf.end());
+
+                string palindrome = "";
+                if(len % 2 == 0){
+                    palindrome = firstHalf + secondHalf;
+                }else{
+                    palindrome = firstHalf + secondHalf.substr(1);
+                }
+
+                long long palindromeNum = stoll(palindrome);
+                string baseK = convertToBaseK(palindromeNum, k);
+
+                if(isPalindrome(baseK)){
+                    sum += palindromeNum;
+                    n--;
+                    if(n == 0){
+                        break;
+                    }
+                }
+            }
+            len++;
+        }
+        return sum;
+    }
+};
